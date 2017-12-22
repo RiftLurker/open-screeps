2024908dff61de700d9035e8607740b5eeb997df
Leo Friedrichs <lf@kirchbergerknorr.de>
2017-12-22 11:56:33 +0100

    Exclude template files from getting picked up by other tools

--- generator/index.ts
+++ generator/index.ts
@@ -19,14 +19,23 @@ export = class extends Generator {
 				message: 'What\'s your name?',
 			},
 		]).then(props => {
-
 			const vars = {
 				moduleName: props.moduleName,
 				moduleDescription: props.moduleDescription,
 				moduleAuthor: props.moduleAuthor,
 			};
 
-			this.fs.copyTpl(`${this.templatePath()}`, this.destinationPath('src', props.moduleName), vars);
+			this.destinationRoot(this.destinationPath('src', props.moduleName));
+
+			this.fs.copyTpl(this.templatePath(), this.destinationPath(), vars);
+
+			const mv = (from: string, to: string) => {
+				return this.fs.move(this.destinationPath(from), this.destinationPath(to), vars);
+			};
+
+			mv('_package.json', 'package.json');
+			mv('_readme.md', 'readme.md');
+			mv('_tsconfig.json', 'tsconfig.json');
 		});
 	}
 }

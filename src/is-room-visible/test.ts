138d4d6fba199fbc134c5007e428feebe8c7b301
Leo "PostCrafter" Friedrichs <dev@postcrafter.de>
2017-12-23 17:19:29 +0100

    feat(is-room-visible): add module `is-room-visible`
    fix #3

---
+++ src/is-room-visible/test.ts
@@ -0,0 +1,28 @@
+import test from 'ava';
+
+import { isRoomVisible } from './index';
+
+declare const global: any;
+
+// @todo stub creep with sinon
+// @todo spy if .owner.username is actually accessed
+
+function stubGame(...rooms: string[]) {
+	type RoomsStub = {
+		[roomName: string]: boolean;
+	};
+
+	global.Game = rooms.reduce((game, roomName) => {
+		game.rooms[roomName] = true;
+		return game;
+	}, {
+		rooms: {} as RoomsStub
+	});
+}
+
+test('Should only return true if the object `Game.rooms` contains the specified key', t => {
+	stubGame('E2S7');
+	t.true(isRoomVisible('E2S7'));
+	t.false(isRoomVisible('tutorial'));
+	t.false(isRoomVisible('sim'));
+});
